38de0d9e1d84df22db417ffb509799e1617f3100
Dennislee:) <55692615+dennislee928@users.noreply.github.com>
2026-05-23 14:50:58 +0800

    Auto-commit: Project implementation in progress

--- src/quantum/__init__.py
+++ src/quantum/__init__.py
@@ -1,8 +1,20 @@
 from .circuits.risk_circuit import RiskScoringCircuit, create_simple_risk_circuit
 from .algorithms.ensemble_risk import EnsembleRiskScorer
-from .qiskit.risk_algorithms import QuantumRiskAlgorithms
-from .cuda_q.backend import CUDABackend
 from .utils.entropy import QuantumEntropyGenerator
+
+try:
+    from .qiskit.risk_algorithms import QuantumRiskAlgorithms
+except ImportError:
+    class QuantumRiskAlgorithms:
+        def __init__(self, *args, **kwargs):
+            raise ImportError("Qiskit is not installed. Run: pip install qiskit")
+
+try:
+    from .cuda_q.backend import CUDABackend
+except ImportError:
+    class CUDABackend:
+        def __init__(self, *args, **kwargs):
+            raise ImportError("CUDA-Q is not installed.")
 
 __all__ = [
     'RiskScoringCircuit', 'create_simple_risk_circuit',
